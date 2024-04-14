c9131318e72f6f6fcb56c783b0f9ea1b0a3d9e1c
andrewdreamer13 <isaevandrey9@gmail.com>
2024-04-14 19:09:15 +0300

    first commit

---
+++ modules/app.js
@@ -0,0 +1,55 @@
+import { firstPage } from "./data.js";
+import { secondPage } from "./data.js";
+import { thirdPage } from "./data.js";
+
+const langSelect = document.querySelector("#lang");
+let currentPathName = window.location.pathname;
+let currentText = {};
+let lang;
+let currentLang;
+
+function checkPagePathName() {
+  switch (currentPathName) {
+    case "/index.html":
+      currentText = firstPage;
+      break;
+    case "/html.html":
+      currentText = secondPage;
+      break;
+    case "/css.html":
+      currentText = thirdPage;
+      break;
+
+    default:
+      currentText = secondPage;
+      break;
+  }
+}
+checkPagePathName();
+
+langSelect.addEventListener("change", () => {
+  lang = langSelect.value;
+  location.href = window.location.pathname + "#" + lang;
+  localStorage.setItem("language", lang);
+  document.querySelector("title").textContent = currentText["unit"][lang];
+  for (let key in currentText) {
+    const element = document.querySelector(`[data-lang=${key}]`);
+    if (element) {
+      element.textContent = currentText[key][lang];
+    }
+  }
+});
+
+window.addEventListener("load", () => {
+  currentLang = localStorage.getItem("language");
+  location.href = window.location.pathname + "#" + currentLang;
+  document.querySelector("title").textContent =
+    currentText["unit"][currentLang];
+  for (let key in currentText) {
+    const element = document.querySelector(`[data-lang=${key}]`);
+    if (element) {
+      element.textContent = currentText[key][currentLang];
+    }
+  }
+  langSelect.value = currentLang;
+});
---
+++ modules/data.js
@@ -0,0 +1,99 @@
+export const firstPage = {
+  'unit': {
+    'en': 'multilanguage website',
+    'ua': 'багатомовний сайт',
+    'ru': 'мультиязычный сайт'
+  },
+  'headerNav1': {
+    'en': 'programing language',
+    'ua': 'мова програмування',
+    'ru': 'язык програмирования'
+  },
+  'headerNav2': {
+    'en': 'markup language',
+    'ua': 'мова розмітки',
+    'ru': 'язык разметки'
+  },
+  'headerNav3': {
+    'en': 'style language',
+    'ua': 'мова стилів ',
+    'ru': 'язык стилей'
+  },
+  'sectionTitle1': {
+    'en': 'Something about JS',
+    'ua': 'Дещо про JS',
+    'ru': 'Кое-что о JS'
+  },
+  'sectionText1': {
+    'en': 'JavaScript (JS) is a lightweight interpreted (or just-in-time compiled) programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles.',
+    'ua': 'JavaScript (JS) — це полегшена інтерпретована (або своєчасно скомпільована) мова програмування з першокласними функціями. Хоча він найбільш відомий як мова сценаріїв для веб-сторінок, багато середовищ без браузерів також використовують його, наприклад Node.js, Apache CouchDB і Adobe Acrobat. JavaScript — це багатопарадигмальна, однопотокова, динамічна мова на основі прототипу, яка підтримує об’єктно-орієнтований, імперативний і декларативний стилі (наприклад, функціональне програмування).',
+    'ru': 'JavaScript (JS) — это легковесный интерпретируемый (или оперативно компилируемый) язык программирования с первоклассными функциями. Хотя он наиболее известен как язык сценариев для веб-страниц, его также используют многие небраузерные среды, например Node.js, Apache CouchDB и Adobe Acrobat. JavaScript — это многопарадигмальный однопоточный динамический язык на основе прототипов, поддерживающий объектно-ориентированный, императивный и декларативный (например, функциональное программирование) стили.'
+  }
+}
+
+export const secondPage = {
+  'unit': {
+    'en': 'multilanguage website',
+    'ua': 'багатомовний сайт',
+    'ru': 'мультиязычный сайт'
+  },
+  'headerNav1': {
+    'en': 'programing language',
+    'ua': 'мова програмування',
+    'ru': 'язык програмирования'
+  },
+  'headerNav2': {
+    'en': 'markup language',
+    'ua': 'мова розмітки',
+    'ru': 'язык разметки'
+  },
+  'headerNav3': {
+    'en': 'style language',
+    'ua': 'мова стилів ',
+    'ru': 'язык стилей'
+  },
+  'sectionTitle1': {
+    'en': 'Something about HTML',
+    'ua': 'Дещо про HTML',
+    'ru': 'Кое-что о HTML'
+  },
+  'sectionText1': {
+    'en': ' HTML (HyperText Markup Language) is the code that is used to structure a web page and its content. For example, content could be structured within a set of paragraphs, a list of bulleted points, or using images and data tables. As the title suggests, this article will give you a basic understanding of HTML and its functions.',
+    'ua': 'HTML (мова розмітки гіпертексту) — це код, який використовується для структурування веб-сторінки та її вмісту. Наприклад, вміст можна структурувати в межах набору абзаців, списку маркованих пунктів або використовувати зображення та таблиці даних. Як випливає з назви, ця стаття дасть вам базове розуміння HTML та його функцій.',
+    'ru': 'HTML (Hypertext Markup Language) - это код, который используется для структурирования и отображения веб-страницы и её контента. Например, контент может быть структурирован внутри множества параграфов, маркированных списков или с использованием изображений и таблиц данных. Как видно из названия, эта статья даст вам базовое понимание HTML и его функций.'
+  }
+}
+
+export const thirdPage = {
+  'unit': {
+    'en': 'multilanguage website',
+    'ua': 'багатомовний сайт',
+    'ru': 'мультиязычный сайт'
+  },
+  'headerNav1': {
+    'en': 'programing language',
+    'ua': 'мова програмування',
+    'ru': 'язык програмирования'
+  },
+  'headerNav2': {
+    'en': 'markup language',
+    'ua': 'мова розмітки',
+    'ru': 'язык разметки'
+  },
+  'headerNav3': {
+    'en': 'style language',
+    'ua': 'мова стилів ',
+    'ru': 'язык стилей'
+  },
+  'sectionTitle1': {
+    'en': 'Something about CSS',
+    'ua': 'Дещо про CSS',
+    'ru': 'Кое-что о CSS'
+  },
+  'sectionText1': {
+    'en': ' Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media.',
+    'ua': 'Каскадні таблиці стилів (CSS) — це мова таблиць стилів, яка використовується для опису подання документа, написаного в HTML або XML (включаючи такі діалекти XML, як SVG, MathML або XHTML). CSS описує, як елементи мають відображатися на екрані, на папері, у мовленні чи на інших носіях.',
+    'ru': 'Cascading Style Sheets (CSS) — это язык иерархических правил (таблиц стилей), используемый для представления внешнего вида документа, написанного на HTML или XML (включая различные языки XML, такие как SVG и XHTML. CSS описывает, каким образом элемент должен отображаться на экране, на бумаге, голосом или с использованием других медиа средств.'
+  }
+}
+
